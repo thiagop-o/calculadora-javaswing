950806f57f8d79b0b972303764c9ce45472d5e37
Thiago Oliveira <devfs.thiago.oliveira@gmail.com>
2021-05-21 03:03:18 -0300

    Projeto de Calculadora com JSwing

---
+++ src/br/com/thiagocrow/calc/visao/Botao.java
@@ -0,0 +1,22 @@
+package br.com.thiagocrow.calc.visao;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton {
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setFont(new Font("courier", Font.PLAIN, 25));
+		setOpaque(true);
+		setBackground(cor);
+		setAlignmentX(CENTER_ALIGNMENT);
+		setAlignmentY(CENTER_ALIGNMENT);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+	}
+}
